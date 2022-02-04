dbaba78736bf36d0686d5d1e49089d4a0591fb3f
Draco9990 <51196669+Draco9990@users.noreply.github.com>
2022-02-04 02:51:53 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,16 +2,28 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Win32;
+
 
 namespace _VS__CSharp__ESO_Launcher_Closer
 {
     class Program
     {
+        private static void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            string path;
+            path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            if (rk.GetValue("ESO Launcher Closer") == null) rk.SetValue("ESO Launcher Closer", path);
+        }
 
         static void Main(string[] args)
         {
+            SetStartup();
             bool shouldClose = true;
             Process activeGame = null;
+
             while (true)
             {
                 Process[] processes = Process.GetProcesses();
