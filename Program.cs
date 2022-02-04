eb86f0c1ebb1bebb5bd50cbdcc57758e49a19668
Draco9990 <51196669+Draco9990@users.noreply.github.com>
2022-02-04 01:10:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace _VS__CSharp__ESO_Launcher_Closer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool shouldClose = true;
+            Process activeGame = null;
+            while (true)
+            {
+                Process[] processes = Process.GetProcesses();
+                foreach (Process p in processes)
+                {
+                    if (!string.IsNullOrEmpty(p.MainWindowTitle))
+                    {
+                        if(!shouldClose && p.MainWindowTitle == "Elder Scrolls Online")
+                        {
+                            shouldClose = true;
+                            activeGame = p;
+                        }
+                        else if(shouldClose && p.MainWindowTitle == "Launcher")
+                        {
+                            shouldClose = false;
+                            p.Kill();
+                            p.Dispose();
+                            SystrayRefresher.RefreshTrayArea();
+                            if (activeGame != null) activeGame.WaitForExit();
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
