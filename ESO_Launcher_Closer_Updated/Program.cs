92f5a7028b99a563e7ea4dc75e5d353fa37f1a9d
Draco9990 <51196669+Draco9990@users.noreply.github.com>
2022-02-04 19:21:17 +0100

    Initial Upload
    Signed-off-by: Tygrtraxx <4270354+Tygrtraxx@users.noreply.github.com>

---
+++ ESO_Launcher_Closer_Updated/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ESO_Launcher_Closer
+{
+    class Program
+    {
+        private static void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            string path = Path.ChangeExtension(Application.ExecutablePath, ".exe");
+            if (rk.GetValue("ESO Launcher Closer") == null) rk.SetValue("ESO Launcher Closer", path);
+        }
+
+        static void Main(string[] args)
+        {
+            SetStartup();
+            while (true)
+            {
+                Process[] games = Process.GetProcessesByName("eso64");
+                if (games.Length > 0)
+                {
+                    Process[] launchers = Process.GetProcessesByName("Bethesda.net_Launcher");
+                    foreach (var launcher in launchers)
+                    {
+                        launcher.Kill();
+                        launcher.Dispose();
+                        SystrayRefresher.RefreshTrayArea();
+                    }
+                    games[0].WaitForExit();
+                }
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
