ef94f1d76a1b707fbe153825d489aae1ddfe2e35
Tygrtraxx <4270354+Tygrtraxx@users.noreply.github.com>
2024-05-22 14:21:17 -0400

    Update Program.cs
    Program is a bit more interactive with indicating it's working, to the user

--- ESO_Launcher_Closer_Updated/Program.cs
+++ ESO_Launcher_Closer_Updated/Program.cs
@@ -1,7 +1,7 @@
 ﻿//============================================================================
 // Name        : ESO Launcher Closer Updated
 // Author(s)   : Draco9990 and rewritten by Tygrtraxx
-// Version     : 2.0.0
+// Version     : 2.1.0
 // Created on  : (2022?)
 // Rewritten on: (05/13/2024)
 // Last Update :
@@ -24,48 +24,147 @@ class Program
     *
     * Description: This function will print out text in a specific color, to the user
     * @var text_input
-    *		The text that will be output by the console later on
-    * @var color
-    *		The color that will be used for the text
+    *	The text that will be output by the console later on
+    * @var custom_type
+    *   Changes the way the text and colors will output to the user based on the input
+    * @var colors_array
+    *	An array that holds all of the colors of the input
+    * @var console_color
+    *	An array that holds all of the enum color values for the text
     *=============================================================================================*/
-    private static void custom_console_text(string text_input, string color)
+    private static void custom_console_text(string text_input, string[] colors_array, int custom_type)
     {
-        //Make sure the color is in lowercase to prevent bugs
-        color = color.ToLower();
-
-        //Convert the color input to a ConsoleColor enum value
-        ConsoleColor consoleColor = ConsoleColor.White; // Default color
-        switch (color)
+        //Make sure the colors are in lowercase to prevent bugs
+        for (int i = 0; i < colors_array.Length; i++)
         {
-            case "red":
-                consoleColor = ConsoleColor.Red;
+            colors_array[i] = colors_array[i].ToLower();
+        }
+
+        //Create an array that will hold all of the color enum values
+        ConsoleColor[] console_color = new ConsoleColor[colors_array.Length];
+
+        //Convert the colors input to a ConsoleColor enum value by going through each of them
+        for (int i = 0; i < colors_array.Length; i++)
+        {
+            switch (colors_array[i])
+            {
+                case "red":
+                    console_color[i] = ConsoleColor.Red;
+                    break;
+                case "green":
+                    console_color[i] = ConsoleColor.Green;
+                    break;
+                case "blue":
+                    console_color[i] = ConsoleColor.Blue;
+                    break;
+                case "yellow":
+                    console_color[i] = ConsoleColor.Yellow;
+                    break;
+                case "cyan":
+                    console_color[i] = ConsoleColor.Cyan;
+                    break;
+                case "magenta":
+                    console_color[i] = ConsoleColor.Magenta;
+                    break;
+                case "white":
+                    console_color[i] = ConsoleColor.White;
+                    break;
+                default:
+                    console_color[i] = ConsoleColor.White;
+                    Console.WriteLine("Invalid color input. Using default color (white).");
+                    break;
+            }
+        }
+
+        //Displays the custom text and color based on the functionality that was entered
+        switch (custom_type)
+        {
+            //Solid color for the entire text
+            case 1:
+
+                //Set the text color and write a single colored message
+                Console.ForegroundColor = console_color[0];
+                Console.WriteLine("\n\n" + text_input + "\n");
+
                 break;
-            case "green":
-                consoleColor = ConsoleColor.Green;
+
+            //Every other letter is a different color in the text
+            case 2:
+
+                //Store each indivdual letter of the text into a character array
+                char[] char_array = text_input.ToCharArray();
+
+                //Go through each character in the character array
+                for (int c = 0; c < char_array.Length;)
+                {
+                    //Allow the user to see the color changes
+                    Thread.Sleep(100);
+
+                    //Write the character with the first color
+                    Console.ForegroundColor = console_color[0];
+                    Console.Write(char_array[c]);
+
+                    //Go to the next character in the array
+                    c++;
+
+                    //Check and make sure we're not already at the end of the array
+                    if (c < char_array.Length)
+                    {
+                        //Write the next character with the second color
+                        Console.ForegroundColor = console_color[1];
+                        Console.Write(char_array[c]);
+
+                        //Go to the next character in the array
+                        c++;
+                    }
+                };
+
+                //Allow the user to see the changes
+                Thread.Sleep(2000);
+
+                //Start at the beginning of the line to make it look like the colors are changing
+                Console.Write("\r");
+
+                //Go through each character in the character array (Opposite colors this time)
+                for (int c = 0; c < char_array.Length;)
+                {
+                    //Allow the user to see the color changes
+                    Thread.Sleep(100);
+
+                    //Write the character with the first color
+                    Console.ForegroundColor = console_color[1];
+                    Console.Write(char_array[c]);
+
+                    //Make sure we move onto the next character in the character_array (Opposite colors this time)
+                    c++;
+
+                    //Check and make sure we're not already at the end of the array
+                    if (c < char_array.Length)
+                    {
+                        //Write the next character with the second color
+                        Console.ForegroundColor = console_color[0];
+                        Console.Write(char_array[c]);
+
+                        //Go to the next character in the array
+                        c++;
+                    }
+                };
+
+                //Start at the beginning of the line to make it look like the colors are changing
+                Console.Write("\r");
+
                 break;
-            case "blue":
-                consoleColor = ConsoleColor.Blue;
-                break;
-            case "yellow":
-                consoleColor = ConsoleColor.Yellow;
-                break;
-            case "cyan":
-                consoleColor = ConsoleColor.Cyan;
-                break;
-            case "magenta":
-                consoleColor = ConsoleColor.Magenta;
-                break;
-            case "white":
-                consoleColor = ConsoleColor.White;
-                break;
+
             default:
-                Console.WriteLine("Invalid color input. Using default color (white).");
+
+                //Tell the user something went wrong
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: Unknown custom font function.");
+
                 break;
         }
 
-        //Set the console color and write a colored message and then reset the color
-        Console.ForegroundColor = consoleColor;
-        Console.WriteLine("\n\n" + text_input + "\n");
+        //Reset the color to the default
         Console.ResetColor();
     }
 
@@ -74,7 +173,7 @@ class Program
     *
     * Description: This function will set a registry key so the program automatcially runs on startup
     * @var action
-    *		The action that will happen next based on the user's input (Create or Delete reg key)
+    *	The action that will happen next based on the user's input (Create or Delete reg key)
     *=============================================================================================*/
     private static void modify_registry(string action)
     {
@@ -103,20 +202,20 @@ class Program
                         crk.SetValue("ESO Launcher Closer", path);
 
                         //Tell the user the key has been successfully added
-                        custom_console_text("Registry key has been added successfully.", "green");
+                        custom_console_text("Registry key has been added successfully.", ["green"], 1);
                     }
 
                     else
                     {
                         //Tell the user the registry key already exists
-                        custom_console_text("The registry key already exists.", "cyan");
+                        custom_console_text("The registry key already exists.", ["cyan"], 1);
                     }
                 }
 
                 else
                 {
                     //Tell the user something went wrong
-                    custom_console_text("Error: Unable to add the registry key. (Invalid path ??)", "red");
+                    custom_console_text("Error: Unable to add the registry key. (Invalid path ??)", ["red"], 1);
                 }
 
                 break;
@@ -141,27 +240,27 @@ class Program
                         drk.DeleteValue("ESO Launcher Closer", false);
 
                         //Tell the user the key has been successfully deleted
-                        custom_console_text("Registry key has been deleted successfully.", "green");
+                        custom_console_text("Registry key has been deleted successfully.", ["green"], 1);
                     }
 
                     else
                     {
                         //Tell the user the registry key doesn't exist
-                        custom_console_text("The registry key doesn't exist.", "yellow");
+                        custom_console_text("The registry key doesn't exist.", ["yellow"], 1);
                     }
                 }
 
                 else
                 {
                     //Tell the user something went wrong
-                    custom_console_text("Error: Unable to add the registry key. (Invalid path ??)", "red");
+                    custom_console_text("Error: Unable to add the registry key. (Invalid path ??)", ["red"], 1);
                 }
 
                 break;
 
             default:
                 //Tell the user something went wrong
-                custom_console_text("Error: Unknown action (Somehow you bypassed create or delete registry key 'actions'??)", "red");
+                custom_console_text("Error: Unknown action (Somehow you bypassed create or delete registry key 'actions'??)", ["red"], 1);
                 break;
         }
     }
@@ -170,8 +269,8 @@ class Program
     * Function: console_loading_animation()
     *
     * Description: This function will give the illusion of loading to the user to show its working
-    *@var total_looped_times
-    *		The amount of times we want to loop the whole animation for loading, to be shown to the user
+    * @var total_looped_times
+    *	The amount of times we want to loop the whole animation for loading, to be shown to the user
     *=============================================================================================*/
     private static void console_load_animation(int total_looped_times)
     {
@@ -240,13 +339,17 @@ class Program
                     {
                         //Start the program for the user
                         case 1:
-                            Console.WriteLine("\nStarting the Program: ");
+                            Console.Clear();
+                            Console.WriteLine("Starting the Program: ");
                             console_load_animation(5);
-                            Console.WriteLine("\n\nProgram is now running.");
+                            Console.Clear();
 
                             //While the program is active, we will find all of the launcher processes and end them
                             while (true)
                             {
+                                //Tell the user the program is running
+                                custom_console_text("Program is now running.", ["magenta", "blue" ], 2);
+
                                 Process[] games = Process.GetProcessesByName("eso64");
                                 if (games.Length > 0)
                                 {
@@ -282,13 +385,13 @@ class Program
                 else
                 {
                     //Tell the user there's an error
-                    custom_console_text("Error: Invalid input. Please enter a number between 1 and 4.", "red");
+                    custom_console_text("Error: Invalid input. Please enter a number between 1 and 4.", ["red"], 1);
                 }
             }
             else
             {
                 //Tell the user that there's an error
-                custom_console_text("Invalid input. Please enter a valid number.", "red");
+                custom_console_text("Invalid input. Please enter a valid number.", ["red"], 1);
             }
         } while (user_choice < 1 || user_choice > 4);
 
